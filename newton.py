279ce280f34a9790439857b0daf8b889154e7580
MeiJohnson <meikojeiko@gmail.com>
2019-10-09 15:47:34 +0300

    Added lab Newton

--- newton.py
+++ newton.py
@@ -1,11 +1,14 @@
 import math
 
-def f(x):
-    return x**3-2*x**2+3*x-5
-def df(x):
-    return 3*x**2-4*x+3
-def ddf(x):
-    return 6*x-4
+def f(arg):
+    return arg**3 - 2 * arg**2 + 3 * arg - 5
+
+def df(arg):
+    return 3 * arg**2 - 4 * arg + 3
+
+def ddf(arg):
+    return 6 * arg - 4
+
 a = 1
 b = 2
 e = 0.000001
@@ -13,16 +16,20 @@ e = 0.000001
 cntA = 0
 cntB = 0
 x = a
-while abs(xi-x)>e:
-    cntA+=1
+xi = x-f(x)/df(x)
+cntA += 1
+while abs(xi - x) > e:
     x = xi
-    xi = x-f(x)/df(x)
-print("Answer",xi," ",cntA)
+    xi = x - f(x)/df(x)
+    cntA += 1
+print("Answer", xi,"Count of cycles", cntA)
 
 x = b
-while abs(xi-x)>e:
-    cntB+=1
+xi = x - f(x)/df(x)
+cntB += 1
+while abs(xi - x) > e:
     x = xi
-    xi = x-f(x)/df(x)
+    xi = x - f(x)/df(x)
+    cntB += 1
 
-print("answer",xi," ",cntB)
+print("Answer", xi,"Count of cycles", cntB)
